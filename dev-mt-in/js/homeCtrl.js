412c330fddd245d8d3f0c73a2cd597028ef027fb
SGMartineau <winteralce@gmail.com>
2016-02-17 10:41:41 -0700

    did the second angular drill and day 3 of the weeklong project

--- dev-mt-in/js/homeCtrl.js
+++ dev-mt-in/js/homeCtrl.js
@@ -1,46 +1,45 @@
-angular.module('devMtIn').controller('homeCtrl', function($scope, profileService) {
-    
+angular.module('devMtIn').controller('homeCtrl', function ($scope, profileService) {
+
     $scope.checkForProfile = function () {
         var profileId = JSON.parse(localStorage.getItem('profileId'));
         if (profileId) {
-            profileService.checkForProfile(profileId.profileId).then(function(profile) {
+            profileService.checkForProfile(profileId.profileId).then(function (profile) {
                 $scope.myProfile = profile.data;
-            }).catch(function(err) {
+            }).catch(function (err) {
                 console.error(err);
             });
         }
     }
-    
+
     $scope.checkForProfile();
-    
-    
+
+
     $scope.sortOptions = [{
-        display: 'Ascending',
-        value: false
+            display: 'Ascending',
+            value: false
     },
-    {
-        display: 'Descending',
-        value: true
+        {
+            display: 'Descending',
+            value: true
     }
     ];
-    
+
     profileService.serviceTest();
-    
+
     $scope.editing = false;
-    
+
     $scope.saveProfile = function (profile) {
+        console.log(profile);
         profileService.saveProfile(profile);
     }
-    
-    $scope.deleteProfile = function() {
-  profileService.deleteProfile()
-  .then(function(deletedProfile) {
-    localStorage.removeItem('profileId');
-    $scope.myProfile = {};
-  })
-  .catch(function(err) {
-    console.error(err);
-  });
-}
-    
+
+    $scope.deleteProfile = function () {
+        profileService.deleteProfile().then(function (deletedProfile) {
+                localStorage.removeItem('profileId');
+                $scope.myProfile = {};
+            }).catch(function (err) {
+                console.error(err);
+            });
+    }
+
 });

05d559cadfd2fcddddf18e33094741cc541f47cd
Scott Martineau <winteralce@gmail.com>
2016-03-02 18:20:05 -0700

    day 1 of the weeklong node project done

--- devmtzon/server.js
+++ devmtzon/server.js
@@ -1,6 +1,14 @@
-// TODO: import express and initialize the app
+var express = require('express'),
+    bodyParser = require('body-parser'),
+    _ = require('lodash'),
+    app = express(),
+    port = 8000;
+
 
 app.use(express.static(__dirname + '/public'));
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
 
 app.listen(port, function () {
